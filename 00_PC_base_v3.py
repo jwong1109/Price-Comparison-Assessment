75b220ed305bd1aaa893ab5de8a37954743005f2
jwong1109 <83558222+jwong1109@users.noreply.github.com>
2022-05-19 10:23:34 +1200

    Base Component

--- 00_PC_base_v3.py
+++ 00_PC_base_v3.py
@@ -1,4 +1,4 @@
-# Added 01_user_budget_trial3_v2
+# Added 02_comparing_units_v3_trial2
 
 # Import Statements
 
@@ -18,6 +18,18 @@ def float_checker(question):
                   "decimals)")
 
 
+# Unit comparison function
+def get_unit():
+    choice_error = "Sorry, please enter a valid unit (eg. mL, L, kg, g)\n "
+    valid_units_list = [["mL", "ml", "millilitres", "1"], ["L", "litres", "2"],
+                        ["kg", "kilograms", "3"], ["g", "grams", "4"]]
+    chosen_unit = input("Enter a valid unit for comparison: ")
+    for item in valid_units_list:
+        if chosen_unit in item:
+            return item[0]
+    print(choice_error)
+
+
 # ******** Main Routine ********
 
 # Set up dictionaries / lists needed to hold data
@@ -26,7 +38,6 @@ def float_checker(question):
 # show instructions if needed
 
 # Loop to get details
-
     # Get user budget (between 1 and 500)
     MINIMUM_BUDGET = 1
     MAXIMUM_BUDGET = 500
@@ -40,6 +51,11 @@ def float_checker(question):
     print(f"Budget = ${budget:,.2f}")
 
     # Get unit choice for comparison (mL, L, kg, g)
+    unit_choice = None
+    while unit_choice is None:
+        unit_choice = get_unit()
+
+    print(f"Unit = {unit_choice}")
 
     # Get input for product name
 
